fb878898dbe004600725da8e7099812b21bf0b65
Hinokas <felipefrancox3@hotmail.com>
2024-07-31 14:05:06 -0300

    Projeto Calculo de gasolina e álcool
    Parceria com o curso sujeito programador

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function calcular (event){
+    event.preventDefault();
+
+    let alcoolInput = document.getElementById("alcool").value;
+    let gasolinaInput = document.getElementById("gasolina").value;
+    let contentResult = document.getElementById ("content-result");
+    let textResult = document.getElementById ("text-result");
+
+    let gasolinaSpan = document.getElementById ("gasolina-result");
+    let alcoolSpan = document.getElementById ("alcool-result");
+
+    let calculo =(alcoolInput / gasolinaInput);
+
+    if(calculo <0.7){
+
+        textResult.innerHTML = "compensa usar Álcool";
+    }else{
+
+        textResult.innerHTML = "compensa usar Gasolina";
+    }
+
+    gasolinaSpan.innerHTML = "Gasolina R$ " + gasolinaInput;
+    alcoolInput.innerHTML = "Álcool R$ " + alcoolInput;
+
+    
+    contentResult.classList.remove("hide")
+
+}
